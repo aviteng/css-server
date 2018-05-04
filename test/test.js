cba41be39d7c5616218efbda3f71c4c3c0bfa9b4
Mehmet Baker <mehmet.baker@gmail.com>
2018-05-04 14:32:11 +0300

    Module path should be passed to the middleware function
    Signed-off-by: Mehmet Baker <mehmet.baker@gmail.com>

--- test/test.js
+++ test/test.js
@@ -5,8 +5,10 @@ const path = require('path');
 const app = express();
 
 const staticPath = path.join(__dirname, './public');
+const modulePath = path.join(__dirname, '../node_modules');
+
 app.use(express.static('../node_modules'));
-app.use(cssServer(staticPath));
+app.use(cssServer(staticPath, modulePath));
 app.use(express.static(staticPath));
 
 app.listen(3000);
